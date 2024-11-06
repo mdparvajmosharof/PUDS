e551b1d5f0843d72b5044746f9671ed436dc74e5
OMI786I <sohrawardy1998@gmail.com>
2024-11-07 02:03:45 +0600

    added navigate to top botton

--- FRONTEND/src/Layout/Root.jsx
+++ FRONTEND/src/Layout/Root.jsx
@@ -3,16 +3,25 @@ import Banner from "../Shared/Banner";
 import Navbar from "../Shared/Navbar";
 import Footer from "../Shared/Footer";
 import Headline from "../Pages/Home/Headline";
+import ScrollToTop from "react-scroll-to-top";
+import MyCustomSvg from "../assets/arrow.svg";
 
 const Root = () => {
-	return (
-		<div className="max-w-6xl mx-auto">
-			<Headline></Headline>
-			<Navbar></Navbar>
-			<Outlet></Outlet>
-			<Footer></Footer>
-		</div>
-	);
+  return (
+    <div className="max-w-6xl mx-auto">
+      <Headline></Headline>
+      <Navbar></Navbar>
+      <ScrollToTop
+        smooth
+        component={<img src={MyCustomSvg} alt="Scroll to top" />}
+        style={{
+          borderRadius: "50%",
+        }}
+      />
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Root;
